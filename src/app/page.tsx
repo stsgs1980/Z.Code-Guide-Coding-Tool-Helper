f5ed20a2b3e554638007cf22b7775e08fac553d9
Z User <z@container>
2026-05-11 21:23:24 +0000

    fix: md:pr-24 lg:pr-24 right padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,7 +94,7 @@ function GuideContent() {
       </div>
 
       {/* Content sections */}
-      <main className="md:ml-14 md:pr-54 lg:pr-54 w-full px-4 sm:px-6 pb-20 flex-1 break-words">
+      <main className="md:ml-14 md:pr-24 lg:pr-24 w-full px-4 sm:px-6 pb-20 flex-1 break-words">
         <QuickStartSection />
         <HelperSection />
         <ZCodeSection />
